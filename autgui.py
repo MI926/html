cd866acbd900ace8d18ae8925c2f326c1b2a3844
MANI GUPTA <mg9263645@gmail.com>
2024-09-10 15:58:45 +0530

    no nothing

--- autgui.py
+++
@@ -1,7 +0,0 @@
-import pyautogui
-import time
-x, y = pyautogui.position()
-print(x, y)
-#time.sleep(5)
-#pyautogui.moveTo(929, 359)
-pyautogui.click(1129, 331)
--- pywin.py
+++
@@ -1,2 +0,0 @@
-from pywinauto.application import Application
-app = Application().connect(title_re="Google Translate - Chromium", class_name="Chromium")
